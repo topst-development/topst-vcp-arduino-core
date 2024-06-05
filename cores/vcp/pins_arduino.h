d56640fd246c9e4cbb18aef50f5769fbec47153b
Hwang-Seung-Jin <sjhwang@huconn.com>
2024-06-05 16:28:52 +0900

    VCP Board > FMU LED
    FMU LED : Arduino LED_BUILTIN

--- cores/vcp/pins_arduino.h
+++ cores/vcp/pins_arduino.h
@@ -1,8 +1,8 @@
 #ifndef Pins_Arduino_h
 #define Pins_Arduino_h
 
-static const uint8_t LED_BUILTIN = 2;                        // Status LED.
+//static const uint8_t LED_BUILTIN = 2;                        // Status LED.
 #define BUILTIN_LED LED_BUILTIN                              // backward compatibility
-#define LED_BUILTIN 16 //hsj
+#define LED_BUILTIN 316 // K16
 
 #endif /* Pins_Arduino_h */
