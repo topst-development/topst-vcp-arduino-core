607a7e5994a502a81a8d6f0826930281ad04976f
Daekeun Kang <dkkang@huconn.com>
2024-12-22 11:26:57 +0000

    fix: Update pin control functions for TOPST_VCP_G version
    Add new implementation of pinCFG, pinSET, pinREAD functions under
    TOPST_VCP_G define flag. Modified to use direct GPIO control method.

--- cores/vcp/pins_vcp.c
+++ cores/vcp/pins_vcp.c
@@ -12,7 +12,8 @@
   reserved -> ..
 */
 #endif
-
+// #define TOPST_VCP_G // TODO: remove this
+#ifndef TOPST_VCP_G
 uint8_t pinCFG(uint32_t vcpPin, uint32_t vcpPinM)
 {
     uint8_t ret;
@@ -412,3 +413,52 @@ int pinREAD(uint32_t vcpPin)
 
     return (level) ? HIGH : LOW;
 }
+#else
+uint8_t pinCFG(uint32_t Pin, uint32_t PinMode)
+{
+    uint8_t ret;
+    uint32_t GPIO_PinMode;
+    /* INPUT: 0, OUTPUT: 1 INPUT_PULLUP: 2 */
+    if (PinMode == INPUT_PULLUP)
+    {
+        GPIO_PinMode = GPIO_INPUT | GPIO_INPUTBUF_EN | GPIO_PULLUP;
+    } else if (PinMode == INPUT)
+    {
+        GPIO_PinMode = GPIO_INPUT | GPIO_INPUTBUF_EN | GPIO_NOPULL;
+    } else if (PinMode == OUTPUT)
+    {
+        GPIO_PinMode = GPIO_OUTPUT;
+    }
+
+    ret = (uint8_t)GPIO_Config(Pin, GPIO_PinMode);
+    if (ret != 0) // 0 means success, so return 0 if failed
+    {
+        return 0;
+    }
+
+    return ret;
+}
+
+uint8_t pinSET(uint32_t Pin, uint32_t Value)
+{
+    uint8_t ret;
+
+    ret = (uint8_t)GPIO_Set(Pin, Value);
+    if (ret != 0) // not 0 means failed, so return FALSE
+    {
+        return FALSE;
+    } else {
+        return TRUE;
+    }
+}
+
+int pinREAD(uint32_t Pin)
+{
+    uint8_t level = 0;
+
+    level = GPIO_Get(Pin);
+
+    return (level) ? HIGH : LOW;
+}
+
+#endif
